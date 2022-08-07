33bb4c5bf3dbdadb3b59e4a238b98b3355d94924
yanyu <yuyan802@outlook.com>
2022-08-08 01:10:03 +1000

    dotnet config

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,12 +15,12 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://yyds:yyds@cluster0.bgtpkdq.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://yyds:yyds@cluster0.bgtpkdq.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5001;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
